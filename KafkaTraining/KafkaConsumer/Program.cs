3a73a963604290a1a0d54e0a18867952739bda85
Mandour <ahmedmandur@gmail.com>
2023-05-10 00:08:42 +0300

    Add kafka producer and consumer

---
+++ KafkaTraining/KafkaConsumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using Confluent.Kafka;
+using Shared;
+
+const string topic = "training-kafka";
+const string groupId = "test_group";
+const string bootstrapServers = "localhost:9092";
+
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = bootstrapServers,
+    GroupId = groupId,
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+try
+{
+    using var consumerBuilder = new ConsumerBuilder<Ignore, string>(config).Build();
+    consumerBuilder.Subscribe(topic);
+    var cancelToken = new CancellationTokenSource();
+
+    try
+    {
+        while (true)
+        {
+            var consumer = consumerBuilder.Consume
+                (cancelToken.Token);
+            var employee = JsonSerializer.Deserialize<EmployeeRequest>(consumer.Message.Value);
+            Console.WriteLine($"Processing Employee Name: {employee.Name}");
+        }
+    }
+    catch (OperationCanceledException)
+    {
+        consumerBuilder.Close();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine("Hello, World!");
---
+++ KafkaTraining/Shared/EmployeeRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared;
+
+public class EmployeeRequest
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Jobtitle { get; set; }
+
+    public string Address { get; set; }
+}
